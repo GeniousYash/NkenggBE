398d08b8029272a0481f7e263e923cf854a4b9d0
Yash Singh <singhyash3012@gmail.com>
2024-08-18 01:11:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 require('dotenv').config();
+
+app.use(cors());
 
 const app = express();
 const port = process.env.PORT || 3000;
